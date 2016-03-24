3d44694677d6747cff80bfbf6301fd2693390f28
kommidi <kommidi@yahoo-inc.com>
2016-03-24 00:00:01 -0700

    initial code

--- chart.js
+++ chart.js
@@ -1,4 +1,8 @@
 
+var dataFile = "data1.tsv";
+
+setInterval(function () {
+d3.select("body").html('');
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
@@ -23,38 +27,42 @@ var svg = d3.select("body").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", type, function(error, data) {
-  if (error) throw error;
 
-  x.domain(data.map(function(d) { return d.item; }));
-  y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    dataFile = (dataFile === "data.tsv") ? "data1.tsv" : "data.tsv";
+    d3.tsv(dataFile, type, function(error, data) {
+      if (error) throw error;
 
-  svg.append("g")
-      .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Count");
+      x.domain(data.map(function(d) { return d.item; }));
+      y.domain([0, d3.max(data, function(d) { return d.count; })]);
 
-  svg.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.item); })
-      .attr("width", x.rangeBand())
-      .attr("y", function(d) { return y(d.count); })
-      .attr("height", function(d) { return height - y(d.count); });
-});
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
 
-function type(d) {
-  d.count = +d.count;
-  return d;
-}
+      svg.append("g")
+          .attr("class", "y axis")
+          .call(yAxis)
+        .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Count");
+
+      svg.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.item); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.count); })
+          .attr("height", function(d) { return height - y(d.count); });
+    });
+
+    function type(d) {
+      d.count = +d.count;
+      return d;
+    }
+}, 1000);
